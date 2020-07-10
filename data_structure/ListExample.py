8e829147ed6a5b8e22cd029f1c8ef341817f004a
Tom Fan <tomboxfan@hotmail.com>
2020-07-10 23:11:57 +0800

    add ListExample

---
+++ data_structure/ListExample.py
@@ -0,0 +1,53 @@
+# Creat a list ------------------------------------
+
+a = ["Apple", 'Orange', 'Banana', "Pear"]
+print(a)
+print(a[1])
+
+b = ['tiger',
+     'elephant',
+     'snake',
+     'shark']
+print(b)
+
+c = list("I love python!")
+print(c)
+
+
+# List can be heterogeneous (Contains all kinds of objects)
+a[1] = 7
+a.append([1, 2, 3])
+a.append(1.234)
+a.append('Sunday')
+a.append(True)
+a.append(0xf)
+print(a)
+
+fruits = ["Pear", 'Orange', 'Banana', "Apple"]
+
+#list.pop(index)
+fruit1 = fruits.pop(0)
+fruit2 = fruits.pop(1)
+print(fruit1) # Pear
+print(fruit2) # Banana
+print(fruits) # ['Orange', 'Apple']
+
+
+fruits = ["Pear", 'Orange', "Pear", 'Banana', "Apple"]
+
+#list.remove(value)
+fruits.remove("Pear")
+print(fruits)  # ['Orange', 'Pear', 'Banana', 'Apple']
+
+fruits.remove("Pear")
+print(fruits)  # ['Orange', 'Banana', 'Apple']
+
+
+
+
+#sort fruits
+fruits.sort()
+print(fruits)
+
+
+
