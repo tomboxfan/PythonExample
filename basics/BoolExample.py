8c0592610a00634c8fa83a2b777497774461fa75
Tom Fan <tomboxfan@hotmail.com>
2020-07-18 09:59:21 +0800

    update

--- basics/BoolExample.py
+++ basics/BoolExample.py
@@ -5,10 +5,13 @@ c = bool(-1)
 d = bool(0.0)
 e = bool(0.207)
 f = bool(-1.117)
+
+# You can print multiple values, separated by comma
 print(a, b, c, d, e, f)
 
 
 # empty list is False, non empty list is True -----
+# ignore this if 还没有学list
 g = bool([])
 h = bool([1,5,9])
 print(g, h)
@@ -18,3 +21,11 @@ print(g, h)
 i = bool('')
 j = bool("Spam")
 print(i, j)
+
+
+#   >   <   !=      ==  用法
+valueEquals1 =  2 == 3
+valueEquals2 =  2 < 3
+valueEquals3 =  2 > 3
+valueEquals4 =  2 != 3
+print(valueEquals1, valueEquals2, valueEquals3, valueEquals4)
