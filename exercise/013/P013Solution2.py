f29a7a48de79536e8c2b70991aee7ed825af0809
tomboxfan <Passw0r1>
2020-09-12 10:18:33 +0800

    update

---
+++ exercise/013/P013Solution2.py
@@ -0,0 +1,11 @@
+# From Yinuo
+
+# from 100 to 999
+for i in range(100, 1000):
+
+    a = int(i / 100)
+    b = int((i - a * 100) / 10)
+    c = i - a * 100 - b * 10
+
+    if a ** 3 + b ** 3 + c ** 3 == i:
+        print(i)
---
+++ exercise/013/P013Solution3.py
@@ -0,0 +1,9 @@
+# Natali
+
+for a in range(1, 10):
+    for b in range(10):
+        for c in range(10):
+            i = 100 * a + 10 * b + c
+            if a ** 3 + b ** 3 + c ** 3 == i:
+                print(i)
+
